3f9ea5ac1cdecb18fc924a9c08aef46788001923
Romanhardy99 <kulenergystreet@mail.ru>
2024-07-20 16:13:49 +0400

    add 4 task practice

--- Task1A/obj/Debug/net8.0/Task1A.AssemblyInfo.cs
+++ Task1A/obj/Debug/net8.0/Task1A.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1A")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc160873d5bde986e8ba4b04e45a1eae8efbd9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1A")]
--- Task1B/obj/Release/net8.0/Task1B.AssemblyInfo.cs
+++ Task1B/obj/Release/net8.0/Task1B.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task1A")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task1B")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc160873d5bde986e8ba4b04e45a1eae8efbd9d")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task1A")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task1A")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task1B")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task1B")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
