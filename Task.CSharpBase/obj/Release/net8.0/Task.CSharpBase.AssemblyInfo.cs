f3a6584ccbb49451afd0311600edfa2aed62af08
Romanhardy99 <kulenergystreet@mail.ru>
2024-07-20 16:03:25 +0400

    other file1

--- Task.CSharpBase/obj/Release/net8.0/Task.CSharpBase.AssemblyInfo.cs
+++ Task.CSharpBase/obj/Release/net8.0/Task.CSharpBase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task.CSharpBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7743f03b282bd22296efba4bf8aca7edd377f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task.CSharpBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task.CSharpBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
