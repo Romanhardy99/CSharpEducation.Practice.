ad91029b283ad0f09381e2815e56d45d1e136970
Romanhardy99 <kulenergystreet@mail.ru>
2024-07-24 10:05:48 +0400

    Добавлен фильтр. В решении отображается только проект Task1A

--- Task1B/obj/Debug/net8.0/Task1B.AssemblyInfo.cs
+++ Task1B/obj/Debug/net8.0/Task1B.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1B")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f6724de4fab3bb39579a8a620ef4775c5238db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bda0db3df8d537aaa399d0eb5eec4f538825f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1B")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1B")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
