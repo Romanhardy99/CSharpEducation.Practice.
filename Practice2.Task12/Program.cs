b4c4b7f43a2c7b9138766b9a5f9d4d35eef68c5a
Romanhardy99 <kulenergystreet@mail.ru>
2024-08-01 09:49:30 +0400

    add Practice2.Task12

---
+++ Practice2.Task12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Practice2.Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("челикс кекикс, сколь челиксов вывести на экран (ЦИФРУ БРАТ НАПИШИ): ");
+
+           if (!int.TryParse(Console.ReadLine(), out int input) && input < 1)
+            {
+                Console.WriteLine("Введите корректное число!");
+                return;
+            }
+
+            string[] array = new string[input];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Имя нормиса {i + 1}: ");
+                array[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Смари шо накулебасил: ");
+            foreach (string name in array)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
--- Task3/obj/Debug/net8.0/Practice1.Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Practice1.Task3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice1.Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09338bd28b5f56151aa90a20da4f36765f2bed40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b55384169ffa6dc57f8c0e0440031cdfa3b1417")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice1.Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice1.Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
