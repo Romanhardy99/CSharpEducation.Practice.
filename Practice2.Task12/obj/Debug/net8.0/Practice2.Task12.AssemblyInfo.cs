479feee60c2fecc791d9f895a7e462292fbcd700
Romanhardy99 <kulenergystreet@mail.ru>
2024-08-01 11:25:52 +0400

    add Practice2.Task16

--- Practice2.Task12/obj/Debug/net8.0/Practice2.Task12.AssemblyInfo.cs
+++ Practice2.Task12/obj/Debug/net8.0/Practice2.Task12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice2.Task12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dda8e1963a9dc7a6688c105c94f697b939b449f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82355888ca6007d0eacae24294a2f7f75f3cbf14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice2.Task12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice2.Task12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice2.Task16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 15;
+            Console.WriteLine($"Число по ссылке до метода: {num}");
+            Increment(ref num);
+            Console.WriteLine($"Число по ссылке после метода: {num}");
+        }
+
+        static void Increment(ref int n)
+        {
+            n = -n;
+        }
+
+    }
+}
