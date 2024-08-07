94febdaa1944ca492427ea5b1d32ea8556ed99d7
Romanhardy99 <kulenergystreet@mail.ru>
2024-08-07 11:43:09 +0400

    add Practice2.Task20

---
+++ Practice2.Task20/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace Practice2.Task20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random random = new Random();
+
+            int[,] Array = new int[100,100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        Array[i, j] = random.Next(0, 101);
+                    }
+                }
+                
+            }
+
+            int max = Array[0,0];
+
+            int min = Array[0,0];
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j =0; j < 100; j++)
+                {
+                    if (Array[i,j] > max)
+                    {
+                        max = Array[i,j];
+                    }
+                    if (Array[i,j] < min)
+                    {
+                        min = Array[i,j];
+                    }
+                }
+            }
+
+            int difference = max - min;
+
+            Console.WriteLine("Массив: ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            for (int i = 0; i < 100; i++)
+            {
+                for(int j = 0;j < 100; j++)
+                {
+                    Console.Write(Array[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+            Console.WriteLine($"Максимальное значение: {max}");
+            Console.WriteLine($"Минимальное значение: {min}");
+            Console.WriteLine($"Разница между max и min: {difference}");
+        }
+    }
+}
--- Task7/obj/Debug/net8.0/Practice1.Task7.AssemblyInfo.cs
+++ Task7/obj/Debug/net8.0/Practice1.Task7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice1.Task7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb9401ecd1f0ec09d638a2f265168a3e4b490d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c28b05aec93582193511085fec03f64976954c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice1.Task7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice1.Task7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
