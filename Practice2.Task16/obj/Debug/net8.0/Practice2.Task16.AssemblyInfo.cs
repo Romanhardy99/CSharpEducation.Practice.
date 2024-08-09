b2d9c3f55f2a3d77295af099afe7cd60f8db29de
Romanhardy99 <kulenergystreet@mail.ru>
2024-08-09 14:36:33 +0400

    add Practice3.Task8

--- Practice2.Task16/obj/Debug/net8.0/Practice2.Task16.AssemblyInfo.cs
+++ Practice2.Task16/obj/Debug/net8.0/Practice2.Task16.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice2.Task16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a9e879a75d381f3e84443a0fb19d5b5ff0e4a53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc368a5144164e9b5ec298a4aca6627cd80684f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice2.Task16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice2.Task16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Practice3.Task8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Practice3.Task8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите ширинку: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите высотку: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            
+
+            Rectangle rect = new Rectangle(width, height);
+
+            double area = rect.CalculateArea();
+
+            Console.WriteLine($"Площадь прямоугольника: {area}");
+        }
+    }
+
+    struct Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+
+
+    }
+}
