0c790737c7ae16b88a5e916685a1c5291bd060be
Roman Kulyomin <KULenergyStreet@mail.ru>
2024-08-11 11:59:51 +0400

    Добавлена библиотека (Практика 3 часть 2. Задание 1)

---
+++ MyLib/MathHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace MyLib
+{
+    public class MathHelper
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Делить на ноль нельзя");
+            }
+            return a / b;
+        }
+
+    }
+}
--- Practice2.Task20/obj/Debug/net8.0/Practice2.Task20.AssemblyInfo.cs
+++ Practice2.Task20/obj/Debug/net8.0/Practice2.Task20.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice2.Task20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14bec1eb6763c2b3506f9bc8022192d32d917fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2942d3d98c4d4e7696f7503e4c4b2b6551b56cba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice2.Task20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice2.Task20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
